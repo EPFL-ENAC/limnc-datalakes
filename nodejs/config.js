a28633d498bd6fd0ccb4c8c6cf33ce97231db095
ymarcon <yannick.marcon@epfl.ch>
2025-08-21 15:23:56 +0200

    feat: git lfs added to container, default config updated for epfl deployment

---
+++ nodejs/config.js
@@ -0,0 +1,12 @@
+// Change name to config.js
+module.exports = {
+  db_user: "", // Database username (production db)
+  db_host: "", // Database host address (production db)
+  db_database: "", // Database name (production db)
+  db_password: "", // Database password (production db)
+  db_port: 5432, // Database port (production db)
+  apiUrl: "https://datalakes-api.epfl.ch", // URL of this Nodejs app 
+  API_KEY: "", // Set API access key for automated access to protected datasets
+  sendgrid_token: "", // Sendgrid token for sending automated emails
+  default_port: 4000 // Default port
+};
